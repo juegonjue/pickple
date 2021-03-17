86060ef5b66697dff777b619c11cb11f0f8aa723
juegonjue <tojue123@naver.com>
2021-03-17 18:38:08 +0900

    initializing

---
+++ src/main/java/com/se/pickple_api_server/PickpleApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.se.pickple_api_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickpleApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickpleApiServerApplication.class, args);
+	}
+
+}
